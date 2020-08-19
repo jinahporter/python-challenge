46888db7645eeab7da1cfb28ef8ffe81326ef6e9
Jinah Porter <jinahporter@gmail.com>
2020-08-18 22:48:49 -0500

    PyBank main.py assignment - 1, 2, 4, 5 worked

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,37 @@
 import os
 import csv
 
-#csvpath = os.path.join('..', 'Resources', 'election_data.csv')
-csv_file = 'PyBank/Resources/budget_data.csv'
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
-with open(csv_file, 'r') as file_handler:
-     lines = file_handler.read()
-     print(lines)
-     print(type(lines))
+
+months = []
+profit_losses = []
+average_change = []
+profit_change = []
+
+with open(csvpath, 'r') as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter = ',')
+#    print(csvreader)
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+#    print(f"CSV Header: {csv_header}")
+    # Read each row of data after the header
+    for row in csvreader:
+        profit_losses.append(int(row[1]))
+        months.append(str(row[0]))
+        
+
+total_months = len(months)
+
+print(total_months)
+print("Total Profit/Losses: $ " + str(sum(profit_losses)))
+# 1) total number of months, 2)total net profit done (8/17)
+
+for a in range(len(profit_losses)-1):
+    profit_change.append(profit_losses[a + 1] - profit_losses[a])
+
+max_increase = max(profit_change)
+max_decrease = min(profit_change)
+
+print(f"The greatest increase in profits is {max_increase}, and the greatest decrease in profits is {max_decrease}")
