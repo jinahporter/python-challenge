dcab0e58f8b65c6b36f591c58f1dc5f1145c2e86
Jinah Porter <jinahporter@gmail.com>
2020-08-20 22:19:55 -0500

    updated: candidate name (unique values) and vote counts done

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,28 +3,41 @@ import csv
 
 csvpath = os.path.join('Resources', 'election_data.csv')
 
-#with open(csvpath, 'r') as file_handler:
-#     lines = file_handler.read()
-#     print(lines)
-#     print(type(lines))
-
 vote = []
 candidates = []
+candidate_vote = []
+count = 0
+
+vote_pct = []
+
+total_vote = 0
+
 
 with open(csvpath, "r") as csvfile:
      csvreader = csv.reader(csvfile, delimiter = ',')
      csv_header = next(csvreader)
 
      for row in csvreader:
-        vote.append(int(row[0]))
-        candidates.append(row[2])
-
-list_of_candidates = set(candidates)
-new_list_of_candidates = list(list_of_candidates)
+          vote.append(int(row[0]))
+          
+          candidate_name = row[2]
+          if candidate_name in candidates:
+               candidate_index = candidates.index(candidate_name)
+               candidate_vote[candidate_index] = candidate_vote[candidate_index] + 1
+          else:
+               candidates.append(candidate_name)
+               candidate_vote.append(1)
 
 
 total_num_vote = len(vote)
 
-print(total_num_vote)
-print(new_list_of_candidates)
+#print(total_num_vote)
 
+
+print("Election Results")
+print("-------------------------------------")
+print(f"Total Votes: {total_num_vote} ")
+print("-------------------------------------")
+
+for a in range(len(candidates)):
+     print(f"{candidates[a]} : {candidate_vote[a]}")
