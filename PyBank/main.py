e962a766b4bf5e2982ce710ad49f04c1bf0bbb7a
Jinah Porter <jinahporter@gmail.com>
2020-08-22 21:37:48 -0500

    PyBank update: found max inc. and dec. in profit months.    ---- updated the .txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -26,18 +26,27 @@ total_months = len(months)
 print("Financial Analysis")
 print("---------------------------------------------")
 print(f"Total Months: {total_months}")
-print("Total: $ " + str(sum(profit_losses)))
+print("Total: $" + str(sum(profit_losses)))
 # 1) total number of months, 2)total net profit done (8/17)
 
 # 3, 4, 5 start below
 for a in range(len(profit_losses)-1):
     profit_change.append(profit_losses[a + 1] - profit_losses[a])
+#print(profit_change)
+#print(sum(profit_change))
 
 average_change = (sum(profit_change) / len(profit_change))
+print(f"Average Change: ${average_change}")
+
 max_increase = max(profit_change)
 max_decrease = min(profit_change)
 
-print(f"Average Change: ${average_change}")
-print(f"Greatest Increase in Profits: ($ {max_increase})")
-print(f"Greatest Decrease in Profits: ($ {max_decrease})")
-# 3) 4) & 5) greatest increase/decrease done
+#find max. min. months label
+max_increase_months = months[profit_change.index(max_increase)+1]
+#print(max_increase_months)
+max_decrease_months = months[profit_change.index(max_decrease)+1]
+#print(max_decrease_months)
+
+print(f"Greatest Increase in Profits: {max_increase_months} ($ {max_increase})")
+print(f"Greatest Decrease in Profits: {max_decrease_months} ($ {max_decrease})")
+# 4) & 5) greatest increase/decrease done
